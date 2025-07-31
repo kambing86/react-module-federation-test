54f0bdef94197bd145261fdbe935848c11afd1c1
Chua Kang Ming <chuakangming@spgroup.com.sg>
2025-07-31 14:54:17 +0800

    change to use @module-federation/rsbuild-plugin

--- host/rsbuild.config.ts
+++ host/rsbuild.config.ts
@@ -1,26 +1,27 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
-import path from 'node:path';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import {
+	createModuleFederationConfig,
+	pluginModuleFederation,
+} from "@module-federation/rsbuild-plugin";
+import path from "node:path";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  tools: {
-    rspack: (config, { appendPlugins }) => {
-      appendPlugins([
-        new ModuleFederationPlugin({
-          name: 'host',
-          remotes: {
-            remote1: 'remote1@http://localhost:3001/mf-manifest.json',
-            remote2: 'remote2@http://localhost:3002/mf-manifest.json',
-          },
-          shared: ['react', 'react-dom'],
-          runtimePlugins: [path.join(__dirname, "./src/runtime-plugin/retry.ts")],
-        }),
-      ]);
-    },
-  },
-  plugins: [pluginReact()],
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		pluginReact(),
+		pluginModuleFederation(
+			createModuleFederationConfig({
+				name: "host",
+				remotes: {
+					remote1: "remote1@http://localhost:3001/mf-manifest.json",
+					remote2: "remote2@http://localhost:3002/mf-manifest.json",
+				},
+				shared: ["react", "react-dom"],
+				runtimePlugins: [path.join(__dirname, "./src/runtime-plugin/retry.ts")],
+			}),
+		),
+	],
 });
